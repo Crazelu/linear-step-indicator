bb730c77e054798c3fcd5ce1447a4698fd2b0818
Crazelu <eberelucky64@gmail.com>
2021-07-05 22:25:09 +0100

    Changes primarySwatch

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Step Indicator Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
       home: StepIndicatorPageViewDemo(),
     );
