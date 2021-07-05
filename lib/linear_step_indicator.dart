e5876be9d31c6655454a86caf9c4027fbd16e550
Crazelu <eberelucky64@gmail.com>
2021-07-05 21:25:38 +0100

    Adds StepIndicatorPageView

--- lib/linear_step_indicator.dart
+++ lib/linear_step_indicator.dart
@@ -1,3 +1,4 @@
 library linear_step_indicator;
 
 export 'src/linear_step_indicator.dart';
+export 'src/step_indicator_page_view.dart';
---
+++ lib/src/step_indicator_page_view.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:linear_step_indicator/src/linear_step_indicator.dart';
+
+class StepIndicatorPageView extends StatelessWidget {
+  final int steps;
+  final PageController controller;
+  final List<Widget> children;
+  final double spacing;
+
+  const StepIndicatorPageView({
+    Key? key,
+    required this.steps,
+    required this.controller,
+    required this.children,
+    this.spacing = 20,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          LinearStepIndicator(steps: steps, controller: controller),
+          SizedBox(height: spacing),
+          Expanded(
+            child: PageView(
+              children: children,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
