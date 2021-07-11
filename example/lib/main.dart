efec3b5c95d4c0b42e21cd8622a76625d97ee2aa
Crazelu <eberelucky64@gmail.com>
2021-07-12 00:00:52 +0100

    Adds assets

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: StepIndicatorPageViewDemo(),
+      home: StepIndicatorDemo(),
     );
   }
 }
